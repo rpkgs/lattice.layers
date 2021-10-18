6e8571158467eedf42e6e5486d4c143e31cf426f
Dongdong Kong <kongdd@users.noreply.github.com>
2021-10-18 13:59:23 +0800

    add background color for grid.text in `layer_statistic`

--- R/layer_statistic.R
+++ R/layer_statistic.R
@@ -2,26 +2,29 @@
 #' 
 #' @inheritParams grid::grid.text
 #' @param FUN one of [matrixStats::weightedMedian()], [matrixStats::weightedMean()]
-#' @param ... other parameters to [grid.text()]
+#' @param ... other parameters to [grid::gpar()]
 #' @export
 layer_statistic <- function(
     x = 0.1, y = 0.9,
     digit = 2, include.sd = TRUE, 
     name = "u", unit = "", FUN = weightedMedian,
-    hjust = 0, vjust = 1, cex = 1,
+    hjust = 0, vjust = 1, cex = 1, fill = "white",
     # cex = 1.2, adj = c(0, 0),
     ...)
 {
-    gp = gpar(fontfamily = get_family(), cex = cex)
+    gp = gpar(fontfamily = get_family(), cex = cex, fill = fill, ...)
     dots = listk(digit, include.sd, unit, FUN)
     layer({
-        params <- listk(z = z[subscripts]) %>% c(dots)
-        label <- do.call(spatial_meansd, params)
-        text.params$label = label
-        do.call(grid.text, text.params)
+        gp <- param$gp
+        param$label <- do.call(spatial_meansd, listk(z = z[subscripts]) %>% c(dots))
+        
+        grid.rect(x = param$x, y = param$y, 
+            hjust = param$hjust, vjust = param$vjust,
+            width = stringWidth(param$label)*gp$cex, height = stringHeight(param$label)*gp$cex,
+            gp = gpar(fill = param$gp$fill, col = "transparent"))
+        do.call(grid.text, param)
         # grid.text(label, dots3)
-    }, data = listk(dots,
-                    text.params = listk(x, y, hjust, vjust, gp)))
+    }, data = listk(dots, param = listk(x, y, hjust, vjust, gp)))
 }
 
 #' Statistic of median±sd or median
